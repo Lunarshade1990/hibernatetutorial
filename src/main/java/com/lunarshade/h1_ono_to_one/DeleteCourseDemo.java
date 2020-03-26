6fd224a4d43b1cee39cc0595283fbc41331a9582
LunarShade <na.yada1990@gmail.com>
2020-03-26 08:05:07 +0300

    ManyToMany - create student and course

--- src/main/java/com/lunarshade/h1_ono_to_one/DeleteCourseDemo.java
+++ src/main/java/com/lunarshade/h1_ono_to_one/DeleteCourseDemo.java
@@ -1,11 +1,13 @@
 package com.lunarshade.h1_ono_to_one;
 
-import com.lunarshade.h1_ono_to_one.entity.Course;
-import com.lunarshade.h1_ono_to_one.entity.Instructor;
-import com.lunarshade.h1_ono_to_one.entity.InstructorDetail;
+import com.lunarshade.h1_ono_to_one.entity.*;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class DeleteCourseDemo {
     public static void main(String[] args) {
@@ -13,17 +15,18 @@ public class DeleteCourseDemo {
                 .configure("hibernate.cfg.xml") //Имя не обязательно, если используется имя по-умолчанию
                 .addAnnotatedClass(Instructor.class)
                 .addAnnotatedClass(InstructorDetail.class)
+                .addAnnotatedClass(Student.class)
                 .addAnnotatedClass(Course.class)
+                .addAnnotatedClass(Review.class)
                 .buildSessionFactory();
 
         Session session = sessionFactory.getCurrentSession();
         try {
             session.beginTransaction();
-
-            int id = 13;
+//            Удаляется из таблицы course И из джоин тэйбл!
+            int id = 11;
             Course course = session.get(Course.class, id);
             session.delete(course);
-
             session.getTransaction().commit();
             System.out.println("Сохранение завершено");
         } catch (Exception e) {
